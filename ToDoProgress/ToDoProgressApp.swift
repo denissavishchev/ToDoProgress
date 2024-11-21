ee115205df5a4d40b9423143de7d3c16732ccacf
Denis Savishchev <denis.savishchev@gmail.com>
2024-11-21 21:51:20 +0100

    Initial Commit

---
+++ ToDoProgress/ToDoProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoProgressApp.swift
+//  ToDoProgress
+//
+//  Created by Devis on 21/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
